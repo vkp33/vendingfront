e7f6dd65aafc7e052a9c5a0d4552221a9f6cbacc
Varad Pai <vkp33@scarletmail.rutgers.edu>
2021-03-05 15:56:51 -0500

    Added some comments

--- src/components/drinkItem.js
+++ src/components/drinkItem.js
@@ -14,26 +14,28 @@ class DrinkItem extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  //on hovering over drink item we flip to other side
   handleHover(event) {
     event.preventDefault();
     this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
   }
 
+  //on click of purchase button, send request to api to update stock amount and download file
   handleClick(event){
     event.preventDefault();
     const fileName = JSON.stringify(this.props.drinks.name);
     const data = JSON.stringify(this.props.drinks);
     const apiUrl = 'https://vending-machine-project.herokuapp.com/api/drink/purchase/' + JSON.stringify(this.props.drinks.id);
     this.props.updateStock(fileName, data, apiUrl);
+    //if stock is 3 or less we timeout the purhcase button for 2 seconds each time to make sure there's no extra json files downloaded if someone spam clicks
     if(JSON.stringify(this.props.drinks.available_stock) < 4)
     {
       this.setState({
         isButtonDisabled: true
       });
-
-      // **** here's the timeout ****
       setTimeout(() => this.setState({ isButtonDisabled: false }), 2000);
     }
+    //plays audio of bottle dropping 
     const audioUrl = process.env.PUBLIC_URL + '/bottledrop.wav';
     const audio = new Audio(audioUrl);
     audio.play();
